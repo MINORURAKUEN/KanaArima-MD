6160730376d34d8b0304b86623b00f57d2d3f448
Jhon <165972049+MINORURAKUEN@users.noreply.github.com>
2026-03-11 17:55:56 -0500

    Update downloader-yt-dlp-dla.js

--- plugins/downloader-yt-dlp-dla.js
+++ plugins/downloader-yt-dlp-dla.js
@@ -1,23 +1,35 @@
 import { exec } from "child_process"
 import fs from "fs"
 
-let handler = async (m, { conn, args }) => {
+let handler = async (m, { conn, args, command }) => {
 
-if (!args[0]) throw "❗ Envia un link de YouTube"
+if (!args[0]) {
+throw "❗ Envia un link de YouTube"
+}
 
 let url = args[0]
 let file = `./tmp/${Date.now()}.mp3`
 
 await conn.reply(m.chat, "⏳ Descargando audio...", m)
 
-exec(`yt-dlp --js-runtimes node -f bestaudio -x --audio-format mp3 -o "${file}" ${url}`, async (err) => {
+exec(`yt-dlp -f bestaudio -x --audio-format mp3 -o "${file}" ${url}`, async (err) => {
 
 if (err) {
 console.log(err)
-return conn.reply(m.chat, "❌ Error descargando audio", m)
+return conn.reply(m.chat, "❌ Error descargando el audio", m)
 }
 
-await conn.sendFile(m.chat, file, "audio.mp3", "🎵 Aquí tienes tu audio", m)
+if (!fs.existsSync(file)) {
+return conn.reply(m.chat, "❌ No se pudo generar el mp3", m)
+}
+
+await conn.sendFile(
+m.chat,
+file,
+"audio.mp3",
+"🎵 Aquí tienes tu audio",
+m
+)
 
 fs.unlinkSync(file)
 
@@ -27,5 +39,6 @@ fs.unlinkSync(file)
 
 handler.command = ['ytmp3','yta']
 handler.tags = ['downloader']
+handler.help = ['ytmp3 <url>']
 
 export default handler
